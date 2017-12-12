32a5b6816b5f1b3ecef8698c91452f6624684009
Anderson <asipe@redventures.com>
2017-12-12 12:38:06 -0500

    adding file structure

--- index.php
+++ index.php
@@ -1,1 +1,21 @@
-echo "Hello World"
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Holiday Decorations Store</title>
+</head>
+<body>
+    <header>
+        
+    </header>
+    <section>
+        
+    </section>
+    <footer>
+        
+    </footer>
+    
+</body>
+</html>
